9384aea2b96f57f6fe56759dddbba343cf9dd711
Web-impuls <wysiwygwebbuilder.offershop@gmail.com>
2026-03-11 18:22:59 +0300

    fix: remove react-native-worklets dependency incompatible with RN 0.76.6

--- babel.config.js
+++ babel.config.js
@@ -1,20 +1,13 @@
 module.exports = function (api) {
   api.cache(true);
   
-  const isTest = process.env.NODE_ENV === 'test';
-  
   return {
     presets: [
       ['babel-preset-expo', { jsxImportSource: 'nativewind' }],
       'nativewind/babel',
     ],
-    plugins: isTest ? [] : [
-      [
-        'react-native-reanimated/plugin',
-        {
-          globals: ['__scanCodes'],
-        },
-      ],
+    plugins: [
+      'react-native-reanimated/plugin',
     ],
   };
 };
